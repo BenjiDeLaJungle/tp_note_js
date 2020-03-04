0d0f1a10533b64548a7f3c5d3e3f7aa9cb5d0114
Renard Benjamin <benjamin-renard@outlook.fr>
2020-03-04 16:45:09 +0100

    test route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
+function creer_article(){
+
+}
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
@@ -8,6 +12,16 @@ app.get('/', function (req, res) {
 
 app.get('/tg', function (req, res) {
   res.send('TG SAMUEL!')
+})
+
+app.get('/creer', function (req, res) {
+  function(){
+  	res.send('crééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééééé')
+  }
+})
+
+app.get('/liste', function (req, res) {
+  res.lister_article()
 })
 
 app.listen(PORT, function () {
