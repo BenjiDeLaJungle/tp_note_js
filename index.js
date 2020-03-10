7f701e2a84279f563e8657591439e4c1fd1e8868
Renard Benjamin <benjamin-renard@outlook.fr>
2020-03-10 15:26:35 +0100

    test loool

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
+
 const PORT = process.env.PORT || 5000 // this is very important
-function creer_article(){
+function test(){
 	console.log("test")
 }
 
@@ -10,13 +11,14 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/tg', function (req, res) {
-  res.send('TG SAMUEL!')
+app.get('/test', function (req, res) {
+  res.send(test())
 })
 
 app.get('/creer', function (req, res) {
   res.send()
   creer_article()
+  console.log("test")
 })
 
 app.get('/liste', function (req, res) {
