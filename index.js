114dff0e7ee8f7ef19652bda96630d4ca39e8de8
BenjiDeLaJungle <benjamin-renard@outlook.fr>
2020-03-20 14:01:15 +0100

    test jwt

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-const express = require('express')
+const express = require ('express')
+const cors = require ('cors')
+const bodyParser = require ('body-parser')
+const Sequelize = require ('sequelize')
+const epilogue = require ('épilogue')
+const OktaJwtVerifier = require ('@ okta / jwt-verifier')
 const app = express()
 
 const PORT = process.env.PORT || 5000 // this is very important
-function test(){
-	console.log("test")
-}
 
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.get('/test', function (req, res) {
-  app.execute(test())
-})
-
-app.get('/creer', function (req, res) {
-  res.send()
-  creer_article()
-  console.log("test")
-})
-
-app.get('/liste', function (req, res) {
-  res.send(req)
-})
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
+
+
+
+const oktaJwtVerifier = nouveau OktaJwtVerifier ({
+  clientId: '{votreClientId}',
+  émetteur: 'https: // {yourOktaDomain} .com / oauth2 / default'
+})
+
+app.use (cors ())
+app.use (bodyParser.json ())
+
+// vérifie le middleware de jeton JWT
+app.use ((req, res, next) => {
+  // demande à chaque requête d'avoir un en-tête d'autorisation
+  if (! req.headers.authorization) {
+    return next (nouvelle erreur ('L'en-tête d'autorisation est obligatoire'))
+  }
+  let parties = req.headers.authorization.trim (). split ('')
+  laissez accessToken = parts.pop ()
+  oktaJwtVerifier.verifyAccessToken (accessToken)
+    .then (jwt => {
+      req.user = {
+        uid: jwt.claims.uid,
+        email: jwt.claims.sub
+      }
+      prochain()
+    })
+    .catch (suivant) // jwt n'a pas vérifié!
+})
+
