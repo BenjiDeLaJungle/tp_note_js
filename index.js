60213a4b7d03e3b7da95e2de6273096e6d71f2e3
BenjiDeLaJungle <benjamin-renard@outlook.fr>
2020-03-22 14:03:31 +0100

    ajout jwt

--- index.js
+++ index.js
@@ -1,52 +1,78 @@
-const express = require('express')
 const bodyParser = require('body-parser')
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const passport = require('passport')
+const passportJWT = require('passport-jwt')
+const secret = 'thisismysecret'
 const urlEncodedParser = bodyParser.urlencoded({ extended: false })
 
 const PORT = process.env.PORT || 5000
+
+const users = [{ email: 'admin@admin.tp', password: 'azertyuiop' }]
+
+const ExtractJwt = passportJWT.ExtractJwt
+const JwtStrategy = passportJWT.Strategy
+
+const jwtOptions = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: secret
+}
+
+const jwtStrategy = new JwtStrategy(jwtOptions, function(payload, next) {
+  // usually this would be a database call:
+  const user = users.find(user => user.email === payload.user)
+
+  if (user) {
+    next(null, user)
+  } else {
+    next(null, false)
+  }
+})
+
+passport.use(jwtStrategy)
+
 const app = express()
 
-app.get('/', function(req, res) {
-  res.send('Hello world !')
+app.get('/public', (req, res) => {
+  res.send('I am public folks!')
+})
+
+app.get('/private', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.send('Hello ' + req.user.email)
 })
 
 app.get('/form', urlEncodedParser, function(req, res) {
   console.log("form")
   console.log(req.body)
   res.send('form')
-  res.send('form2')
 })
+
+app.post('/login', urlEncodedParser, (req, res) => {
+  const email = req.body.email
+  const password = req.body.password
+
+  if (!email || !password) {
+    res.status(401).json({ error: 'Email or password was not provided.' })
+    return
+  }
+
+  // usually this would be a database call:
+  const user = users.find(user => user.email === email)
+
+  if (!user || user.password !== password) {
+    res.status(401).json({ error: 'Email / password do not match.' })
+    return
+  }
+
+  const userJwt = jwt.sign({ user: user.email }, secret)
+
+  res.json({ jwt: userJwt })
+})
+
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
 })
 
 
-/*const oktaJwtVerifier = new OktaJwtVerifier ({
-  clientId: '{votreClientId}',
-  émetteur: 'https: // {yourOktaDomain} .com / oauth2 / default'
-})
-
-app.use (cors ())
-app.use (bodyParser.json ())
-*/
-// vérifie le middleware de jeton JWT
-/*
-app.use ((req, res, next) => {
-  // demande à chaque requête d'avoir un en-tête d'autorisation
-  if (! req.headers.authorization) {
-    return next (new error ('L'en-tête d'autorisation est obligatoire'))
-  }
-  let parties = req.headers.authorization.trim (). split ('')
-  laissez accessToken = parts.pop ()
-  oktaJwtVerifier.verifyAccessToken (accessToken)
-    .then (jwt => {
-      req.user = {
-        uid: jwt.claims.uid,
-        email: jwt.claims.sub
-      }
-      prochain()
-    })
-    .catch (suivant) // jwt n'a pas vérifié!
-})
-*/
 
